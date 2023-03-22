1e1089b715ce99be77fdf23e935083772876ddb0
Rafay Qayyum <49898477+Rafayqayyum@users.noreply.github.com>
2023-03-23 03:01:50 +0500

    fixed bugs

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 import os
-from openai import api_key, Audio, Completion
+from openai import  Audio, Completion
+import openai
 from pytube import YouTube
-import ffmpeg
+import moviepy.editor as mp
 VIDEO_PATH='videos'
 AUDIO_PATH='audios'
-api_key = os.environ.get("OPENAI_API_KEY")
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 def download_video(url):
     try:
         # create a directory to store the video
@@ -16,13 +17,14 @@ def download_video(url):
         out_file= video.download(output_path=VIDEO_PATH)
         # get the video name
         out_file = os.path.basename(out_file)
+        new_out_file=out_file.replace(" ", "_")
         # remove whitespace from video name
-        new_out_file = out_file.replace(" ", "_")
-        os.rename(VIDEO_PATH+'/'+out_file, VIDEO_PATH+'/'+new_out_file)
+        if os.path.exists(os.path.join(VIDEO_PATH,new_out_file)) is False:
+            os.rename(os.path.join(VIDEO_PATH,out_file),os.path.join(VIDEO_PATH,new_out_file))
         # return the video name
         return new_out_file
-    except:
-        return None
+    except Exception as e:
+        print(e)
 
 
 def convert_video_to_audio(video_name):
@@ -30,25 +32,34 @@ def convert_video_to_audio(video_name):
         if os.path.exists(AUDIO_PATH) is False:
             os.mkdir(AUDIO_PATH)
         # create audio file name
-        audio_file_name = AUDIO_PATH+'/'+video_name.split(".")[0] + ".wav"
+        audio_file_name =os.path.join(AUDIO_PATH,video_name.split(".")[0] + ".mp3")
         # convert video to audio
-        stream = ffmpeg.input(VIDEO_PATH+'/'+video_name)
-        stream = ffmpeg.output(stream, audio_file_name, format='mp3')
-        ffmpeg.run(stream)
+        print(video_name)
+        print(audio_file_name)
+        clip = mp.AudioFileClip(os.path.join(VIDEO_PATH,video_name))
+        clip.write_audiofile(audio_file_name)
+        clip.close()
         # remove video file
-        os.remove(video_name)
+        if os.path.exists(os.path.join(VIDEO_PATH,video_name)):
+            os.remove(os.path.join(VIDEO_PATH,video_name))
         # return audio file name
         return audio_file_name
     except Exception as e:
         print(e)
         return None
+    
 def get_transcript(audio_file_name):
     try:
+        print(audio_file_name)
         # get transcript
         audio_file= open(audio_file_name, "rb")
         transcript = Audio.translate("whisper-1", audio_file)
+        audio_file.close()
+        if os.path.exists(audio_file_name):
+            os.remove(audio_file_name)
         return transcript['text']
-    except:
+    except Exception as e:
+        print(e)
         return None
 
 def summarize_video(transcript):
@@ -67,6 +78,7 @@ def summarize_video(transcript):
         frequency_penalty=0.1,
         presence_penalty=0.1)
         return response.choices[0].text.strip()
-    except:
+    except Exception as e:
+        print(e)
         return None
     
